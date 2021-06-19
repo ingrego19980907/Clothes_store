b1aa5c32cd9a6dde2a244b0fde34a5f37d524ef3
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-06-19 22:18:05 +0300

    upload info with database

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import ProductCategory, Product
 
 
 def index(request):
@@ -12,26 +12,14 @@ def index(request):
 
 def products(request):
     context = {
-        "title": "Store-Catalog"
+        "title": "Store-Catalog",
+        "categories": ProductCategory.objects.all(),
+        "products": Product.objects.all(),
+
     }
     return render(request, 'products/products.html',context)
 
 
-def test_context(request):
-    context = {
-        "title": "store",
-        "hearer": "Welcome",
-        "username": 'Ivan',
-        "products": [
-            {"name": "Худи черного цвета с монограммами adidas Originals", "price": 6090},
-            {"name": "Синяя куртка The North Face", "price": 23725},
-            {"name": "Коричневый спортивный oversized-топ ASOS DESIGN", "price": 3390},
-        ],
-        "products_of_promotion": [
-            {"name": "Черный рюкзак Nike Heritage", "price": 2340},
-        ]
-    }
 
-    return render(request, 'products/test_context.html', context)
 
 

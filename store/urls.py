b008c2d0ff40c4b8da46f8211d611f86b2a592c6
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-08-01 21:15:05 +0300

    registration form added

--- store/urls.py
+++ store/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
-
 ]
 
 if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -24,4 +24,14 @@ def login(request):
 
 
 def register(request):
-	return render(request, "users/register.html")
+	if request.method == 'POST':
+		form = UserRegistrationForm(data=request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse("users:login"))
+		else:
+			print(form.errors)
+	else:
+		form = UserRegistrationForm()
+	context = {"form": form}
+	return render(request, "users/register.html", context=context)

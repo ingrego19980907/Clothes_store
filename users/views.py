4ed29c37a2723d0a606b0ca43da8357e7717645a
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-08-01 23:17:06 +0300

    profile form

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -35,3 +35,17 @@ def register(request):
 		form = UserRegistrationForm()
 	context = {"form": form}
 	return render(request, "users/register.html", context=context)
+
+
+def profile(request):
+	if request.method == 'POST':
+		form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('users:profile'))
+		else:
+			print(form.errors)
+	else:
+		form = UserProfileForm(instance=request.user)
+	context = {"form": form}
+	return render(request, "users/profile.html", context=context)

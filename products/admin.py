f55e7993b2a1e270cba91812e2987f76f598ceaf
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-08-13 23:30:12 +0300

    admin changed

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from products.models import ProductCategory, Product, Basket
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
 admin.site.register(Basket)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ("name", "price", "quantity", "category")
+	fields = ("name", "image", "description", "short_description", ("price", "quantity"), "category")
+	ordering = ("name", )
+	search_fields = ("name", "quantity", "category")
+	
+	
+class BasketAdminInLine(admin.TabularInline):
+	model = Basket
+	fields = ("product", "category", "created_timestamp")
+	readonly_fields = ("product", "created_timestamp",)
+	extra = 0
+	

044f1361c168712708986c25fe061889cabad5ef
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-09-07 23:18:58 +0300

    admin user changed

--- products/admin.py
+++ products/admin.py
@@ -9,15 +9,15 @@ admin.site.register(Basket)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
 	list_display = ("name", "price", "quantity", "category")
-	fields = ("name", "image", "description", "short_description", ("price", "high_price"), "quantity", "category",
-	          "size", "color", "factory", "width", "height")
+	fields = ("name", "image", "description", "short_description", "price", "high_price", "quantity", "category",
+	"size", "color", "factory", "width", "height")
 	ordering = ("name", )
 	search_fields = ("name", "quantity", "category")
 	
 	
 class BasketAdminInLine(admin.TabularInline):
 	model = Basket
-	fields = ("product", "category", "created_timestamp")
-	readonly_fields = ("product", "created_timestamp",)
+	fields = ("product", )
+	readonly_fields = ("product", )
 	extra = 0
 	

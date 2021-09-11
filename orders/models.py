8d26206e44ffb27936c40f7dfb8c385fed98f34b
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-09-11 20:48:36 +0300

    models added to admin panel

--- orders/models.py
+++ orders/models.py
@@ -26,12 +26,12 @@ class Order(models.Model):
         return 'Order {}'.format(self.id)
 
     def get_total_cost(self):
-        return sum(product.get_cost() for product in self.products.all())
+        return sum(item.get_cost() for item in self.items.all())
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items')
-    product = models.ForeignKey(Product, related_name='order_items')
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 

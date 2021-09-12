c9595f77f49bab438519e7dd121ece2f072212d4
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-09-12 22:26:29 +0300

    forms and views edited

--- orders/forms.py
+++ orders/forms.py
@@ -1,7 +1,14 @@
-from phonenumber_field.formfields import PhoneNumberField
+# from phonenumber_field.formfields import PhoneNumberField
+from django import forms
+from .models import Order
+
+
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'phone', 'email', 'address', 'postal_code', 'city']
 
 
 
-
-class ClientForm(forms.Form):
-    phone = PhoneNumberField()
+# class ClientForm(forms.Form):
+#     phone = PhoneNumberField()

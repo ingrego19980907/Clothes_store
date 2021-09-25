2756626ba25e15ddff5e03525cb0b85f6c4c4f6a
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-09-25 23:21:23 +0300

    min changes

--- orders/views.py
+++ orders/views.py
@@ -15,7 +15,6 @@ def order_create(request):
                                          product=item['product'],
                                          price=item['price'],
                                          quantity=item['quantity'])
-            # очистка корзины
             cart.clear()
             return render(request, 'orders/order/created.html',
                           {'order': order})

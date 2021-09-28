b90c1a290b7832c4bd34f94a5f6329fac59a7457
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-09-28 23:44:37 +0300

    min changes

--- cart/cart.py
+++ cart/cart.py
@@ -6,9 +6,6 @@ from Products.models import Product
 class Cart(object):
 
     def __init__(self, request):
-        """
-        Инициализируем корзину
-        """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
@@ -17,9 +14,6 @@ class Cart(object):
         self.cart = cart
 
     def add(self, product, quantity=1, update_quantity=False):
-        """
-        Добавить продукт в корзину или обновить его количество.
-        """
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0,'price': str(product.price)}
@@ -36,18 +30,12 @@ class Cart(object):
         self.session.modified = True
 
     def remove(self, product):
-        """
-        Удаление товара из корзины.
-        """
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
 
     def __iter__(self):
-        """
-        Перебор элементов в корзине и получение продуктов из базы данных.
-        """
         product_ids = self.cart.keys()
         # получение объектов product и добавление их в корзину
         products = Product.objects.filter(id__in=product_ids)
@@ -60,15 +48,9 @@ class Cart(object):
             yield item
 
     def __len__(self):
-        """
-        Подсчет всех товаров в корзине.
-        """
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
-        """
-        Подсчет стоимости товаров в корзине.
-        """
         return sum(Decimal(item['price']) * item['quantity'] for item in
                    self.cart.values())
 

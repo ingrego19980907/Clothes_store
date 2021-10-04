724c96c9dbf997407970a4e9fd43ded6fc3c0f1f
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-10-04 23:45:52 +0300

    views cart_add added

--- cart/views.py
+++ cart/views.py
@@ -4,3 +4,15 @@ from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 update_quantity=cd['update'])
+    return redirect('cart:cart_detail')

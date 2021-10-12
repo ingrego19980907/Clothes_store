bff536de3f2bd265caebaccf10e5973da3663da1
Viacheslav Sukhodoiev <suhodoiev@gmail.com>
2021-10-12 23:51:36 +0300

    added changes to forms

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from shop.models import Product
+from products.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 
@@ -17,11 +17,13 @@ def cart_add(request, product_id):
                  update_quantity=cd['update'])
     return redirect('cart:cart_detail')
 
+
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
+
 
 def cart_detail(request):
     cart = Cart(request)
